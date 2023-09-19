91bb97dc7144b19f5f203c632f41813176a0498c
SirrealY <143436688+SirrealY@users.noreply.github.com>
2023-09-18 20:49:54 -0400

    initials were made

--- Assignment1.java
+++ Assignment1.java
@@ -2,6 +2,13 @@
 
 public class Assignment1 {
   public static void main(String[] args) {
-    // Add your code here
+    system.out.println();
+    system.out.println("    sssss     yyyyy   yyyyy            ")
+    system.out.println("    s          yyyy   yyyy            ")
+    system.out.println("    sssss        yy   yy")
+    system.out.println("        s          yyy")
+    system.out.println("        s          yyy")
+    system.out.println("    sssss          yyy")
+ 
   }
 }
