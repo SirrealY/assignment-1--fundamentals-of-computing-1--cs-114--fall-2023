bde3425c93ec27448d1d804234249a5a458cd43d
SirrealY <143436688+SirrealY@users.noreply.github.com>
2023-09-18 21:21:56 -0400

    fahrenheit variable created

--- Assignment1.java
+++ Assignment1.java
@@ -1,14 +1,17 @@
-// Import libraries here
+import java.util.Scanner;  
 
 public class Assignment1 {
   public static void main(String[] args) {
     system.out.println();
-    system.out.println("    sssss     yyyyy   yyyyy            ")
-    system.out.println("    s          yyyy   yyyy            ")
-    system.out.println("    sssss        yy   yy")
-    system.out.println("        s          yyy")
-    system.out.println("        s          yyy")
-    system.out.println("    sssss          yyy")
- 
+    system.out.println("    sssss     yyyyy   yyyyy            ");
+    system.out.println("    s          yyyy   yyyy            ");
+    system.out.println("    sssss        yy   yy");
+    system.out.println("        s          yyy");
+    system.out.println("        s          yyy");
+    system.out.println("    sssss          yyy");
+    
+    Scanner fahrenheit = new Scanner(system.in);
+    system.out.println ("Enter a number in Fahrenheit")
+    int fahrenheit = fahrenheit.nextInt()
   }
 }
