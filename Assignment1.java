9281afa9112c06581c2816e6bbd21b9be48f6b9f
SirrealY <143436688+SirrealY@users.noreply.github.com>
2023-09-19 19:34:17 -0400

    reversed the order of finalstr

--- Assignment1.java
+++ Assignment1.java
@@ -15,22 +15,28 @@ public class Assignment1 {
     System.out.println ("Enter a number in Fahrenheit");
     int fahrenheit = scan.nextInt();
 
-    double Celsius = ((fahrenheit-32)*5)/9;
+    double Celsius = ((fahrenheit-32)*5)/9;//Turns Fahrenheit into Celsius
     System.out.println(Celsius);
 
-    Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);//New Scanner Definetion 
 
     System.out.println("Make a 5-Character string");
 
     String str = sc.nextLine();
 
-    //System.out.println(str);
+    System.out.println(str);//Delete when code is done
 
-    //Removes the first and last characters from a 5 letter word 
+    String finalstr = str.substring(1,4);//Removes the first and last characters from a 5 letter word 
 
-    String finalstr = str.substring(1,4);
+    System.out.println(finalstr);//Delete when code is done
 
-    //System.out.println(finalstr);
+    String reversed = new StringBuilder(finalstr).reverse().toString();
+
+    System.out.println(reversed);
+
+    
+
+  
 
     
 
