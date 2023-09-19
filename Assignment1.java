0e1f81f061ca42b8121e8d49c5437da835013e7a
SirrealY <143436688+SirrealY@users.noreply.github.com>
2023-09-19 13:13:46 -0400

    fixed errors in code

--- Assignment1.java
+++ Assignment1.java
@@ -2,16 +2,31 @@ import java.util.Scanner;
 
 public class Assignment1 {
   public static void main(String[] args) {
-    system.out.println();
-    system.out.println("    sssss     yyyyy   yyyyy            ");
-    system.out.println("    s          yyyy   yyyy            ");
-    system.out.println("    sssss        yy   yy");
-    system.out.println("        s          yyy");
-    system.out.println("        s          yyy");
-    system.out.println("    sssss          yyy");
+    System.out.println("                                            "); // Space at the beginning of Terminal 
+    System.out.println("    sssss     yyyyy   yyyyy            ");
+    System.out.println("    s          yyyy   yyyy            ");
+    System.out.println("    sssss        yy   yy");
+    System.out.println("        s          yyy");
+    System.out.println("        s          yyy");
+    System.out.println("    sssss          yyy");
+    System.out.println("                                              "); //Space at bottom of Terminal 
     
-    Scanner scan = new Scanner(system.in);
-    system.out.println ("Enter a number in Fahrenheit");
+    Scanner scan = new Scanner(System.in);// Scanner Definition 
+    System.out.println ("Enter a number in Fahrenheit");
     int fahrenheit = scan.nextInt();
+
+    double Celsius = ((fahrenheit-32)*5)/9;
+    System.out.println(Celsius);
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Make a 5-Character string");
+
+    String str = sc.nextLine();
+
+    System.out.println(str);
+    
+
+  
   }
 }
