864b29f25548478c6606dbb359a9a0b79c0a957f
SirrealY <143436688+SirrealY@users.noreply.github.com>
2023-09-20 17:03:18 -0400

    final adjustment

--- Assignment1.java
+++ Assignment1.java
@@ -4,10 +4,21 @@ import java.util.Random;
 
 public class Assignment1 {
   public static void main(String[] args) {
+    
+    String finalstr;//Declare finalstr as variable
+
+    int num;//Declare num as variable
+
+    int fahrenheit;
+
+    double celsius;
+
+    Random generate;
+
     System.out.println("                                            "); // Space at the beginning of Terminal 
     System.out.println("     sssss      yyyyy   yyyyy            ");
-    System.out.println("     s          yyyy   yyyy            ");
-    System.out.println("     sssss         yy   yy");
+    System.out.println("     s           yyyy    yyyy            ");
+    System.out.println("     sssss         yy    yy");
     System.out.println("         s          yyy");
     System.out.println("         s          yyy");
     System.out.println("     sssss          yyy");
@@ -15,36 +26,31 @@ public class Assignment1 {
     
     Scanner scan = new Scanner(System.in);// Scanner Definition 
     System.out.println ("Enter a number in Fahrenheit");
-    int fahrenheit = scan.nextInt();
+    fahrenheit = scan.nextInt();
 
-    double Celsius = ((fahrenheit-32)*5)/9;//Turns Fahrenheit into Celsius
-    System.out.println(Celsius);
+    celsius = ((fahrenheit-32)*5)/9;//Turns Fahrenheit into Celsius
 
-    Scanner sc = new Scanner(System.in);//New Scanner Definetion 
+    //Scanner sc = new Scanner(System.in);//New Scanner Definetion 
 
     System.out.println("Make a 5-Character string");
 
-    String str = sc.nextLine();
+    String str = scan.next();
 
     System.out.println("");
 
-    String finalstr = str.substring(1,4);//Removes the first and last characters from a 5 letter word 
+    finalstr = str.substring(1,4);//Removes the first and last characters from a 5 letter word 
 
     String reversed = new StringBuilder(finalstr).reverse().toString();
 
-    Random generate = new Random(); //Initialize Generate 
-
-    int num; 
+    generate = new Random(); //Initialize Generate  
 
     num = generate.nextInt(16384) + 30; //Randomly Generate number from 30-16384
     System.out.println("");//Empty line 
     System.out.println("A random number is being generated...");
     System.out.println("");//Empty line
 
-    System.out.println(Celsius+ reversed + num);// Print all of the variable made
-
-
-    sc.close();
+    System.out.println(celsius+ reversed + num);// Print all of the variable made
+    
     scan.close();
   }
 }
