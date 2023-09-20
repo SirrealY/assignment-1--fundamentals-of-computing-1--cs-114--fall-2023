28c9e8cb13a689e56c645d144c1791363d8976ab
SirrealY <143436688+SirrealY@users.noreply.github.com>
2023-09-20 14:27:51 -0400

    deleted print statements

--- Assignment1.java
+++ Assignment1.java
@@ -5,12 +5,12 @@ import java.util.Random;
 public class Assignment1 {
   public static void main(String[] args) {
     System.out.println("                                            "); // Space at the beginning of Terminal 
-    System.out.println("    sssss     yyyyy   yyyyy            ");
-    System.out.println("    s          yyyy   yyyy            ");
-    System.out.println("    sssss        yy   yy");
-    System.out.println("        s          yyy");
-    System.out.println("        s          yyy");
-    System.out.println("    sssss          yyy");
+    System.out.println("     sssss      yyyyy   yyyyy            ");
+    System.out.println("     s          yyyy   yyyy            ");
+    System.out.println("     sssss         yy   yy");
+    System.out.println("         s          yyy");
+    System.out.println("         s          yyy");
+    System.out.println("     sssss          yyy");
     System.out.println("                                              "); //Space at bottom of Terminal 
     
     Scanner scan = new Scanner(System.in);// Scanner Definition 
@@ -26,15 +26,11 @@ public class Assignment1 {
 
     String str = sc.nextLine();
 
-    System.out.println(str);//Delete when code is done
+    System.out.println("");
 
     String finalstr = str.substring(1,4);//Removes the first and last characters from a 5 letter word 
 
-    System.out.println(finalstr);//Delete when code is done
-
     String reversed = new StringBuilder(finalstr).reverse().toString();
-
-    System.out.println(reversed);//Delete when code is finished
 
     Random generate = new Random(); //Initialize Generate 
 
@@ -43,6 +39,7 @@ public class Assignment1 {
     num = generate.nextInt(16384) + 30; //Randomly Generate number from 30-16384
     System.out.println("");//Empty line 
     System.out.println("A random number is being generated...");
+    System.out.println("");//Empty line
 
     System.out.println(Celsius+ reversed + num);// Print all of the variable made
 
