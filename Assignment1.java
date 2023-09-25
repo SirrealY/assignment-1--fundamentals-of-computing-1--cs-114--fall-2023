9c59ea1bc11f7ba8176df9a5206f864cb9c5cc0f
SirrealY <143436688+SirrealY@users.noreply.github.com>
2023-09-25 16:26:45 -0400

    AFTER PROJECT UPDATES

--- Assignment1.java
+++ Assignment1.java
@@ -5,32 +5,29 @@ import java.util.Random;
 public class Assignment1 {
   public static void main(String[] args) {
     
-    String finalstr;//Declare finalstr as String
+    str finalstr;
 
-    int num;//Declare num as int
+    int num;
 
-    int fahrenheit;//Declare fahrenheit as int 
+    int fahrenheit;
 
-    double celsius;//Declare celsius as double 
+    double celsius;
 
-    Random generate;//Declare generate as random
+    random generate;
 
-    System.out.println("                                            "); // Space at the beginning of Terminal 
+    System.out.println("                                            "); 
     System.out.println("     sssss      yyyyy   yyyyy            ");
     System.out.println("     s           yyyy    yyyy            ");
     System.out.println("     sssss         yy    yy");
     System.out.println("         s           yyy");
     System.out.println("         s           yyy");
     System.out.println("     sssss           yyy");
-    System.out.println("                                              "); //Space at bottom of Terminal 
-    
+    System.out.println("                                              ");
     Scanner scan = new Scanner(System.in);// Scanner Definition 
     System.out.println ("Enter a number in Fahrenheit");
     fahrenheit = scan.nextInt();
 
-    celsius = ((fahrenheit-32)*5)/9;//Turns Fahrenheit into Celsius
-
-    //Scanner sc = new Scanner(System.in);//New Scanner Definetion 
+    celsius = ((fahrenheit-32)*5)/9;//Turns Fahrenheit into Celsius 
 
     System.out.println("Make a 5-Character string");
 
@@ -45,11 +42,11 @@ public class Assignment1 {
     generate = new Random(); //Initialize Generate  
 
     num = generate.nextInt(16384) + 30; //Randomly Generate number from 30-16384
-    System.out.println("");//Empty line 
+    System.out.println("");
     System.out.println("A random number is being generated...");
-    System.out.println("");//Empty line
+    System.out.println("");
 
-    System.out.println(celsius+ reversed + num);// Print all of the variable made
+    System.out.println(celsius + reversed + num);// Print all of the variable made
     
     scan.close();
   }
